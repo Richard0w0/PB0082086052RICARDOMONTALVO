e2deacb0d33ddaafb2bc9a8bd7d9487868827887
Richard0w0 <100311947+Richard0w0@users.noreply.github.com>
2022-03-27 21:42:34 -0600

    Primer Avance Programacion

--- ProyectoProgramacion.cpp
+++ ProyectoProgramacion.cpp
@@ -1,6 +1,3 @@
-// ProyectoProgramacion.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include "pch.h"
 #include <iostream>
 using namespace std;
